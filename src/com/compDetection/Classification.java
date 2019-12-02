ed84ff4317b49eda6c50a8fc8bff32a2cd367f63
matsuda <error359@gmail.com>
2019-12-02 15:56:27 +0900

    change calSLPos()

--- src/com/compDetection/Classification.java
+++ src/com/compDetection/Classification.java
@@ -19,31 +19,43 @@ public class Classification {
     private static IntegrateBlock iB = new IntegrateBlock();
 
     public static void main(String[] args) throws IOException {
-        iB.first();
+        //iB.first();
     }
 
-    public List<List<Integer>> calSLPos(List<List<Integer>> blocks, int clNum) {
-        List<List<Integer>> result = new ArrayList<>();
-        int[][] verLon = new int[clNum][numOfBlock];
-        int[][] horLon = new int[clNum][numOfBlock];
-        //int[][] sh = new int[clNum][numOfBlock];
-        Arrays.fill(verLon, 0);
-        //Arrays.fill(sh, numOfBlock+1);
-
-        for(int i=0; i<numOfBlock; i++) {
-            for(int j=0; j<numOfBlock; j++) {
-                for(int k=0; k<clNum; k++) {
-                    if(k == blocks.get(j).get(i) && k>0 && blocks.get(j).get(i) == blocks.get(j-1).get(i)) {
-                        verLon[k][i]++;
-                    }
-                    if(k == blocks.get(i).get(j) && k>0 && blocks.get(i).get(j) == blocks.get(i).get(j-1)) {
-                        horLon[k][i]++;
-                    }
-                }
-            }
-        }
-        Arrays.sort(verLon);
-    return  result;
-    }
+//    public void calSLPos(List<List<Integer>> blocks, int clNum) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        int[][] verLon = new int[clNum][numOfBlock];
+//        int[][] horLon = new int[clNum][numOfBlock];
+//        //Arrays.fill(verLon, 0);
+//
+//        for(int i=0; i<numOfBlock; i++) {
+//            for(int j=0; j<numOfBlock; j++) {
+//                for(int k=0; k<clNum; k++) {
+//                    if(k == blocks.get(j).get(i) && j>0 && blocks.get(j).get(i) == blocks.get(j-1).get(i)) {
+//                        verLon[k][i]++;
+//                    }
+//                    if(k == blocks.get(i).get(j) && j>0 && blocks.get(i).get(j) == blocks.get(i).get(j-1)) {
+//                        horLon[k][i]++;
+//                    }
+//                }
+//            }
+//        }
+//
+//        for(int i=0; i<verLon.length; i++) {
+//            for(int j=0; j<verLon[i].length; j++) {
+//                System.out.print(verLon[i][j]);
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+//
+//        //System.out.println(verLon);
+//        //System.out.println(horLon);
+//        //Arrays.sort(verLon);
+//        //Arrays.sort(horLon);
+//
+//
+//        //return  result;
+//    }
 
 }
