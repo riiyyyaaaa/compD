add692b793ad9d80e0ac995310183a05ceea333e
matsuda <error359@gmail.com>
2019-12-03 01:43:28 +0900

    fix Classifcation.java

--- src/com/compDetection/Classification.java
+++ src/com/compDetection/Classification.java
@@ -13,8 +13,8 @@ import static com.compDetection.TexGLCM.propertyUtil;
  */
 public class Classification {
 
-    private int numOfBlock = Integer.valueOf(propertyUtil.getProperty("numOfBlock"));
-    private int imageSize = Integer.valueOf(PropertyUtil.getProperty("imageSize"));
+    private static int numOfBlock = Integer.valueOf(propertyUtil.getProperty("numOfBlock"));
+    private static int imageSize = Integer.valueOf(PropertyUtil.getProperty("imageSize"));
 
     private static IntegrateBlock iB = new IntegrateBlock();
 
@@ -22,40 +22,51 @@ public class Classification {
         //iB.first();
     }
 
-//    public void calSLPos(List<List<Integer>> blocks, int clNum) {
-//        List<List<Integer>> result = new ArrayList<>();
-//        int[][] verLon = new int[clNum][numOfBlock];
-//        int[][] horLon = new int[clNum][numOfBlock];
-//        //Arrays.fill(verLon, 0);
-//
-//        for(int i=0; i<numOfBlock; i++) {
-//            for(int j=0; j<numOfBlock; j++) {
-//                for(int k=0; k<clNum; k++) {
-//                    if(k == blocks.get(j).get(i) && j>0 && blocks.get(j).get(i) == blocks.get(j-1).get(i)) {
-//                        verLon[k][i]++;
-//                    }
-//                    if(k == blocks.get(i).get(j) && j>0 && blocks.get(i).get(j) == blocks.get(i).get(j-1)) {
-//                        horLon[k][i]++;
-//                    }
-//                }
-//            }
-//        }
-//
-//        for(int i=0; i<verLon.length; i++) {
-//            for(int j=0; j<verLon[i].length; j++) {
-//                System.out.print(verLon[i][j]);
+    /**
+     * 縦と横それぞれに連続したブロックが何個あるかを数える。
+     * @param blocks
+     * @param clNum
+     */
+    public static void calSLPos(List<List<Integer>> blocks, int clNum) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[][] verLon = new int[clNum][numOfBlock];
+        int[][] horLon = new int[clNum][numOfBlock];
+        //Arrays.fill(verLon, 0);
+
+        for(int i=0; i<numOfBlock; i++) {
+            for(int j=0; j<numOfBlock; j++) {
+                for(int k=0; k<clNum; k++) {
+                    if(k == blocks.get(j).get(i) && j>0 && blocks.get(j).get(i) == blocks.get(j-1).get(i)) {
+                        verLon[k][i]++;
+                    }
+                    if(k == blocks.get(i).get(j) && j>0 && blocks.get(i).get(j) == blocks.get(i).get(j-1)) {
+                        horLon[k][i]++;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Vertical");
+        for(int i=0; i<verLon.length; i++) {
+            for(int j=0; j<verLon[i].length; j++) {
+                System.out.print(verLon[i][j]);
+            }
+            System.out.println();
+        }
+//        System.out.println("Horizon");
+//        for(int i=0; i<horLon.length; i++) {
+//            for(int j=0; j<horLon[i].length; j++) {
+//                System.out.print(horLon[i][j]);
 //            }
 //            System.out.println();
 //        }
 //        System.out.println();
-//
-//        //System.out.println(verLon);
-//        //System.out.println(horLon);
-//        //Arrays.sort(verLon);
-//        //Arrays.sort(horLon);
-//
-//
-//        //return  result;
-//    }
+
+        //return  result;
+    }
+
+    public static void getTexAve() {
+
+    }
 
 }
